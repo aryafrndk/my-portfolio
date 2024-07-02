8e54c1b1b3cd59a916ab0d017ea1f77fa4cf7542
aryafrndk <aryafrandika0408@gmail.com>
2024-07-03 00:45:33 +0700

    Update Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -30,17 +30,17 @@ const Hero = () => {
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/* text */}
-          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+          <div className="flex max-w-[700px] flex-col justify-center mx-auto text-center">
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
               Frontend Developer
             </div>
             <h1 className="h1 mb-4">Hello, my name is Arya Frandika Daulay</h1>
-            <p className="text-black text-lg mb-8 font-light max-w-[490px] mx-auto xl:mx-0 dark:text-white">
+            <p className="text-black text-lg mb-8 font-light max-w-[490px] mx-auto dark:text-white">
               Based in Bandung, Indonesia, I'm passionate about creating
               engaging, digital experiences on the web.
             </p>
             {/* buttons */}
-            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto  mb-12">
               <Link href="/contact">
                 <Button className="gap-x-3 ">
                   Contact Me <Send size={18} />
@@ -55,7 +55,7 @@ const Hero = () => {
               </Button>
             </div>
             {/* socials */}
-            <Socials containerStyles="flex gap-x-6 mx-auto xl:mx-0" />
+            <Socials containerStyles="flex gap-x-6 mx-auto" />
           </div>
           {/* image */}
           <div className="hidden xl:flex relative">{/*Image*/}</div>
