f97897963421091182a59a7b309c83499d673a17
aryafrndk <aryafrandika0408@gmail.com>
2024-07-07 02:08:33 +0700

    Update Animate Style

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,46 +1,21 @@
 "use client";
-import React, { useState } from "react";
-import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import React from "react";
 import ProjectCard from "@/components/ProjectCard";
 import projectData from '@/data/projectData.js';
 
-
-// remove category duplicates
-const uniqueCategories = [
-  "all projects",
-  ...new Set(projectData.map((item) => item.category)),
-];
-
 const Projects = () => {
-  const [categories, setCategories] = useState(uniqueCategories);
-  const [category, setCategory] = useState("all projects");
-
-  const filteredProjects = projectData.filter((project) => {
-    // if category is 'all projects' return all projects, else filter by category
-    return category === "all projects"
-      ? project
-      : project.category === category;
-  });
-
   return (
     <section className="min-h-screen pt-12">
       <div className="container mx-auto">
-        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+        <h2 className="MyProjects section-title mb-8 xl:mb-16 text-center mx-auto animate__animated animate__fadeInDown animate__delay-1s">
           My Projects
         </h2>
-        {/* tabs */}
-        <Tabs defaultValue={category} className="mb-24 xl:48">
-          {/* tabs content */}
-          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
-            {filteredProjects.map((project, index) => {
-              return (
-                <TabsContent value={category} key={index}>
-                  <ProjectCard project={project} />
-                </TabsContent>
-              );
-            })}
-          </div>
-        </Tabs>
+        {/* Project grid */}
+        <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4 mb-[70px] animate__animated animate__fadeInUp animate__delay-1s overflow-hidden">
+          {projectData.map((project, index) => (
+            <ProjectCard key={index} project={project} />
+          ))}
+        </div>
       </div>
     </section>
   );
